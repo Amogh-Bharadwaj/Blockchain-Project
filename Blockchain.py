1678497482b9ec781724b8c326d04169f58e7f38
Amogh-Bharadwaj <amoghrb01@gmail.com>
2021-09-22 21:18:32 +0530

    Completed chain validity function

--- Blockchain.py
+++ Blockchain.py
@@ -41,29 +41,41 @@ class Blockchain:
                 nonce += 1
         return [sha256(test.encode('utf-8')).hexdigest(), nonce]
 
+
     def AddTransaction(self, transaction: Transaction):
-        #checking for key conflicts, so other transactions are not overwritten.
+        #Checking for key conflicts, so other transactions are not overwritten.
         if self.unverified_transaction_pool.get(transaction.uuid, None) != None:
             raise KeyError("Transaction already added or UUID collision detected.")
         self.unverified_transaction_pool[transaction.uuid] = transaction
+    
 
     def UpdateTransactionVote(self, transaction_uuid: str, weight: int):
         if weight <= 0:
             raise ValueError("Weight needs to be a positive integer.")
+
+        #Locate transaction using its uuid.
         if transaction_uuid in self.unverified_transaction_pool.keys():
             transaction = self.unverified_transaction_pool.pop(transaction_uuid)
+
+            #Update its weight based on the voter.
             transaction.current_weight = transaction.current_weight + weight
+
             if transaction.current_weight >= self.weight_for_validate:
-                print("[NOTICE] Transaction " + transaction_uuid + " just reached consensus. Moving to verified pool.")
+                print("[NOTICE] Transaction " + transaction_uuid + " just reached consensus. Moving to verified pool.\n")
+
                 self.verified_transaction_pool[transaction_uuid] = transaction
             else:
                 self.unverified_transaction_pool[transaction_uuid] = transaction 
+
         elif transaction_uuid in self.verified_transaction_pool.keys():
+            #Removing spent transactions.
             transaction = self.verified_transaction_pool.pop(transaction_uuid) 
             transaction.current_weight = transaction.current_weight + weight
+
             self.verified_transaction_pool[transaction_uuid] = transaction
         else:
             raise KeyError("Transaction not currently tracked by blockchain.")    
+
 
     #Method for adding a new block.
     def AddBlock(self, block_data: str):
@@ -74,19 +86,24 @@ class Blockchain:
 
         self.blockchain.append(Block(block_data, new_hash, nonce))
     
-    # REWRITE THIS PLS
+
     def ChainValidity(self, chain):
         #All chains start with the same genesis block.
-        if chain[0].timestamp != 0:
+        if chain[0].block_data != "GENESIS_HASH":
+            print("Error in verifying - ",chain[0].timestamp)
             return False
+        
+        for i in range(1,len(chain)): 
+            proof_test = sha256((str(chain[i - 1].proof_of_work) + chain[i].block_data + str(chain[i].nonce)).encode('utf-8')).hexdigest()
 
-        #Checking if hash of the previous block is equal to the previous hash of the current block.
-        # for i in range(1,len(chain)):
-        #     if (chain[i - 1].proof_of_work != chain[i].previou) or (chain[i].blockHash != MerkleHash(chain[i].transactions).Hashed()) or (sha256(bytes.fromhex(chain[i].proof)).hexdigest()[0:5]!="00000"): #Checking hash and proof of each block as well.
-        #         print("Invalid block!\n")
-        #         return False 
-        # print("Chain is valid.\n")
-        # return True
+            #Checking the proof of work puzzle as well as the proof itself.
+            if proof_test[:self.difficulty]!='0'*self.difficulty and proof_test==chain[i].proof_of_work:
+                print("\n Blockchain is invalid! Corrupt block was created at timestamp ",chain[i].timestamp,".")
+                return False
+
+        print("No discrepancies found. The blockchain has been reverified successfully.\n")
+        return True
+
 
     #Keep replacing the current chain with a longer chain, if there is one.
     def UpdateChain(self,newChain):
@@ -98,16 +115,22 @@ class Blockchain:
            
         else:
             self.blockchain = newChain
+    
 
+    #Carrying out block creation upon finishing transaction validations
     def finalize_verified(self):
          verified_list = list(self.verified_transaction_pool.values())
          cache = []
+
          for transaction in verified_list:
              cache.append(transaction.serialize())
              self.verified_transaction_pool.pop(transaction.uuid)
+              
+             #Once transaction threshold is reached, create a block. 
              if len(cache) == self.max_block_capacity:
                  self.AddBlock(json.dumps(cache))
                  cache.clear()
+         #Create a block for the remaining transactions.
          if len(cache) > 0:
                  self.AddBlock(json.dumps(cache))
                  cache.clear() 
